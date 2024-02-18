b61e26fc082919f243c3a33a729f148f0d7d09a5
annajantzen <aejantz2015@gmail.com>
2024-02-18 14:32:03 -0600

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [
+    function init() {
+        inquirer.prompt ([
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of your application?',
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please describe the purpose of your application and how to use it.'
+    },
+    {
+        name: 'GitHub',
+        type: 'input',
+        message: 'Please enter your GitHub username.'
+    }, 
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Please enter your email address.'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'What will be required for users to install and use the application locally?'
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How will this application be used?'
+    },
+    {
+        name: 'contributors',
+        type: 'input',
+        message: 'Please list any contributors as well as their GitHub username.'
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Please chooose a license type:',
+        choices: ['ISC, GPL v3, MIT, No License']
+    },
+    {
+        name: 'test',
+        type: 'input',
+        message: 'Please descirbe how this application was tested.'
+    }
+]);
+
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
